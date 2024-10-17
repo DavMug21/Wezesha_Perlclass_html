957bddd625b5616701232bc3afcab0d8ff493443
David Mugia <davidmugia21@gmail.com>
2024-10-17 03:06:35 -0700

    Add files via upload

---
+++ Wezesha project/assets/Js/script.js
@@ -0,0 +1,10 @@
+const colorButton=document.getElementById(elementid 'colorButton');
+
+colorButton.addEventListener('click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue'? '4CAF50':'blue'
+
+    }
+
+
+);
+
